ab75c811d6dbd4e99c3425944593570b2fc06540
NOLOVE954048 <shalukumari954048@gmail.com>
2025-01-03 18:21:19 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY KaliaYtOwner
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8100015035:AAEXVFUme8OJ4dv52IqpJL5qMNXqf6H1n1c")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Ninja2399")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6517590211")
